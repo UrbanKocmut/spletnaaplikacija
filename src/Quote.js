dc1d94cb53f3e3654c069acdccf873401abb9648
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-10 20:07:00 +0000

    Deploy UrbanKocmut/spletnaaplikacija to github.com/UrbanKocmut/spletnaaplikacija.git:gh-pages

---
+++ src/Quote.js
@@ -0,0 +1,33 @@
+import './App.css';
+import {Box, Heading, Container,Text} from "@chakra-ui/react"
+import {useEffect, useState} from "react"
+
+export default function Quote() {
+
+    const [quote, setQuote]  = useState({author: '', quote: ''});
+
+
+    useEffect(() => {
+        fetch('http://api.quotable.io/random')
+            .then(resp => resp.json())
+            .then(jsonObj => setQuote({
+                author:jsonObj.author,
+                quote:jsonObj.content,
+            }))
+            .catch((errr) => console.error("Error: "+errr));
+
+    }, []);
+
+    return (
+        <Container>
+            <Box>
+                <Heading>
+                    {quote.quote}
+                </Heading>
+                <Text>
+                    {quote.author}
+                </Text>
+            </Box>
+        </Container>
+    );
+}
