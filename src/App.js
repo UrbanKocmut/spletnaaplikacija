c505dfd9350bfc39137e1330b8ec66bff8bcc0db
Urban Kocmut <urban@kocmut.xyz>
2021-03-10 20:18:55 +0100

    spremembe

--- src/App.js
+++ src/App.js
@@ -1,31 +1,45 @@
 import './App.css';
 import PropTypes from "prop-types";
-import {useState} from "react";
-import { Input,Button,Box,Flex } from "@chakra-ui/react"
+import {useEffect, useState} from "react";
+import { Input,Button,Box,Flex,Text } from "@chakra-ui/react"
 
 export default function App(props, state) {
 
-    const [name, setName] = useState(props.name);
+    const [names, setNames] = useState([props.name]);
     const [input, setInput] = useState();
+
+    useEffect(() => {
+        localStorage.setItem('names', JSON.stringify(names));
+        // eslint-disable-next-line
+    }, []);
+
 
     function inputChanged(event) {
         const {target: {value}} = event;
         setInput(value)
     }
 
-    function changeName(event) {
-        setName(input)
+    function saveName(event) {
+        // debugger;
+        let storedNames = JSON.parse(localStorage.getItem('names'))
+        storedNames.push(input)
+        localStorage.setItem('names', JSON.stringify(storedNames));
+        setNames(storedNames)
     }
 
     return (
-            <Box  w="100%" p={4}>
-                {name}
+        <Box w="100%" p={4}>
+            {names.map((name, index) =>
+                <Text key={index}>
+                    {name}
+                </Text>
+            )}
 
-                <Flex>
-                    <Input onChange={inputChanged} placeholder="Basic usage" />
-                    <Button marginLeft={2} onClick={changeName} colorScheme="blue">Button</Button>
-                </Flex>
-            </Box>
+            <Flex>
+                <Input onChange={inputChanged} placeholder="Name"/>
+                <Button marginLeft={2} onClick={saveName} colorScheme="gray">Save</Button>
+            </Flex>
+        </Box>
     );
 }
 
