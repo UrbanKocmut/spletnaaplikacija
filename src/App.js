8cbc14f5169aad3fd055962f432764aa1d40520f
Urban Kocmut <urban@kocmut.xyz>
2021-03-10 19:11:12 +0100

    spremembe

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import PropTypes from "prop-types";
+import {useState} from "react";
+import { Input,Container,Button,Box,Flex } from "@chakra-ui/react"
 
 export default function App(props, state) {
+
+    const [name, setName] = useState(props.name);
+    const [input, setInput] = useState();
+
+    function inputChanged(event) {
+        const {target: {value}} = event;
+        setInput(value)
+    }
+
+    function changeName(event) {
+        setName(input)
+    }
+
     return (
-            <p className={"tst"}>
-                Hello!
-            </p>
+        <Container>
+            <Box  w="100%" p={4}>
+                {name}
+            </Box>
+            <Flex>
+                <Input onChange={inputChanged} placeholder="Basic usage" />
+                <Button marginLeft={2} onClick={changeName} colorScheme="blue">Button</Button>
+            </Flex>
+        </Container>
     );
 }
+
+App.propTypes = {
+    name: PropTypes.string
+};
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from "@chakra-ui/react"
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <ChakraProvider>
+      <App name={"Janez"}/>
+  </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
