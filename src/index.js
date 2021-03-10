469123f3e1c11ccc322406601db77044974c86f4
Urban Kocmut <urban@kocmut.xyz>
2021-03-10 22:13:05 +0100

    spremembe

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
     <React.StrictMode>
-        <ChakraProvider>
-            <Container>
-                <App name={"Janez"}/>
-                <Divider/>
-                <Quote/>
-            </Container>
-        </ChakraProvider>
+        {/*<ChakraProvider>*/}
+        {/*    <Container>*/}
+        {/*        <App name={"Janez"}/>*/}
+        {/*        <Divider/>*/}
+        {/*        <Quote/>*/}
+        {/*    </Container>*/}
+        {/*</ChakraProvider>*/}
+        <p>hi</p>
     </React.StrictMode>,
     document.getElementById('root')
 );
