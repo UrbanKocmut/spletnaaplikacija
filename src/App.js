0f491d80c887ac5baeaf2b77f710de69aab1b3f3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-10 20:41:15 +0000

    Deploy UrbanKocmut/spletnaaplikacija to github.com/UrbanKocmut/spletnaaplikacija.git:gh-pages

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import PropTypes from "prop-types";
+import {useEffect, useState} from "react";
+import { Input,Button,Box,Flex,Text } from "@chakra-ui/react"
+
+export default function App(props, state) {
+
+    const [names, setNames] = useState([props.name]);
+    const [input, setInput] = useState();
+
+    useEffect(() => {
+        localStorage.setItem('names', JSON.stringify(names));
+        // eslint-disable-next-line
+    }, []);
+
+
+    function inputChanged(event) {
+        const {target: {value}} = event;
+        setInput(value)
+    }
+
+    function saveName(event) {
+        // debugger;
+        let storedNames = JSON.parse(localStorage.getItem('names'))
+        storedNames.push(input)
+        localStorage.setItem('names', JSON.stringify(storedNames));
+        console.log(storedNames)
+        setNames(storedNames)
+    }
+
+    return (
+        <Box w="100%" p={4}>
+            {names.map((name, index) =>
+                <Text key={index}>
+                    {name}
+                </Text>
+            )}
+
+            <Flex>
+                <Input onChange={inputChanged} placeholder="Name"/>
+                <Button marginLeft={2} onClick={saveName} colorScheme="gray">Save</Button>
+            </Flex>
+        </Box>
+    );
+}
+
+App.propTypes = {
+    name: PropTypes.string
+};
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {ChakraProvider, Divider,Container} from "@chakra-ui/react"
+import Quote from "./Quote";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ChakraProvider>
+            <Container>
+                <App name={"Janez"}/>
+                <Divider/>
+                <Quote/>
+            </Container>
+        </ChakraProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
+serviceWorkerRegistration.register();
