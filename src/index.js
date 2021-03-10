bb952ce75a8d2bcb4a90c325767c346404f2d286
Urban Kocmut <urban@kocmut.xyz>
2021-03-10 22:34:17 +0100

    spremembe

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import {ChakraProvider, Divider,Container} from "@chakra-ui/react"
+import Quote from "./Quote";
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
     <React.StrictMode>
-        {/*<ChakraProvider>*/}
-        {/*    <Container>*/}
-        {/*        <App name={"Janez"}/>*/}
-        {/*        <Divider/>*/}
-        {/*        <Quote/>*/}
-        {/*    </Container>*/}
-        {/*</ChakraProvider>*/}
-        <p>hi</p>
+        <ChakraProvider>
+            <Container>
+                <App name={"Janez"}/>
+                <Divider/>
+                <Quote/>
+            </Container>
+        </ChakraProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
